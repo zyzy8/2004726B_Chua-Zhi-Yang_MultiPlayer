0a00bb5e42fc0ad9e7ffaeeb834097bfb0df86c3
zy <chuazhiyang8@gmail.com>
2023-01-31 10:54:43 +0800

    wip sound

--- Assets/Scripts/MultiPlayerLobby.cs
+++ Assets/Scripts/MultiPlayerLobby.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MultiPlayerLauncher : MonoBehaviour
+public class MultiPlayerLobby : MonoBehaviour
 {
   [SerializeField]
   InputField mInputField;
